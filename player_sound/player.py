b6be725648ed1e6aac789c05d08ef0bebf823666
Pamela <pamelan@gec.inatel.br>
2019-04-05 12:26:53 -0300

    implementacao do player de som

--- player_sound/player.py
+++ player_sound/player.py
@@ -1,19 +1,32 @@
-import pygame as pygame
+
 from sound import getSoundOfaHistoy
-from time import sleep
-
-path_sounds = '../sounds_history'
-
-
-x = getSoundOfaHistoy('0','0')
-
-path_sound = '{}/{}/{}'.format(path_sounds,x['folder'],x['name_sound'])
-print(path_sound)
-
-#pygame.init()
-pygame.mixer.init()
-sounda = pygame.mixer.Sound(path_sound)
-sounda.play()
+from playsound import playsound
+import _thread
 
 
+def playOneSound(code_hist,cod_sound):
+    '''
+        Funcao para execuçao do som. Espera finalizar o som para depois começar outro.
+
+        Entrada:
+            - code_hist: Codigo da Historia
+            - cod_sound: Codigo do Som
+    '''
+
+    path_sounds = '../sounds_history'
+    sound = getSoundOfaHistoy(code_hist,cod_sound)
+    path_sound = '{}/{}/{}'.format(path_sounds,sound['folder'],sound['name_sound'])
+    playsound(path_sound)
+
+
+def playTogetherSound(code_hist,cod_sound):
+    '''
+        Funcao para execuçao do som. Pode executar varios som simultaneos, usando thread.
+
+        Entrada:
+            - code_hist: Codigo da Historia
+            - cod_sound: Codigo do Som
+    '''
+
+    _thread.start_new_thread( playOneSound, (code_hist,cod_sound, ) )
 
